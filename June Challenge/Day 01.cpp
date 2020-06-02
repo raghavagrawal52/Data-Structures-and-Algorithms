ebfa87cf5e96caca51474e01aa882cb17edc6561
RAGHAV <58418187+raghavagrawal52@users.noreply.github.com>
2020-06-02 14:02:55 +0530

    Create Day 01.cpp

---
+++ June Challenge/Day 01.cpp
@@ -0,0 +1,25 @@
+// This problem can be solved using recursion
+// And then using a similar concept as that of swapping two variable
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+            return NULL;
+        TreeNode* temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        return root;
+    }
+};
