86bac2b54712b74c6d71d6fe6e2d2d47596d68f7
RAGHAV <58418187+raghavagrawal52@users.noreply.github.com>
2020-06-02 14:04:16 +0530

    Create Day 03.cpp

---
+++ June Challenge/Day 03.cpp
@@ -0,0 +1,17 @@
+// This can be solved by swapping it with the next node
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
