0378d975055334ffe8241b48a238ed8f6cd021d2
RAGHAV <58418187+raghavagrawal52@users.noreply.github.com>
2020-06-12 17:47:27 +0530

    Create Day 12.cpp

---
+++ June Challenge/Day 12.cpp
@@ -0,0 +1,44 @@
+class RandomizedSet {
+    //unordered_set<int> _elems;
+    vector<int> _nums;
+    unordered_map<int, int> _positions;
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(_positions.find(val) != _positions.end())
+            return false;
+        _nums.push_back(val);
+        _positions.insert({val, _nums.size()-1});
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(_positions.find(val) == _positions.end())
+            return false;
+        int pos = _positions[val];
+        _nums[pos] = _nums[_nums.size()-1];
+        _positions[_nums[pos]] = pos;
+        _nums.pop_back();
+        _positions.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return _nums[rand() % _nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
